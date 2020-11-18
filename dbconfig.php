52dae41e5aa5c4b1732331f70a2dad4fca21e376
Aftab Nabi <aftab.pucit@gmail.com>
2020-11-18 16:43:59 +0500

    added missing config n files

--- dbconfig.php
+++ dbconfig.php
@@ -1,7 +1,7 @@
 <?php
-	$dbhost = "localhost";
-	$dbuser = "root";
-	$dbpassword = "";
-	$database = "samproject";
+	$dbhost = "sql212.hostfree.pw";
+	$dbuser = "epree_27217354";
+	$dbpassword = "recall123";
+	$database = "epree_27217354_openproblems";
 	$table_prefix = "pj_";
 ?>
